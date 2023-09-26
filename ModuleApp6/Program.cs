4c0c64c07636d52a25ca1c11aa87f7d403c10292
Nikita Nikita <jassie.jassie@yandex.ru>
2023-09-26 13:52:06 +0300

    Добавьте файлы проекта.

---
+++ ModuleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Rectangle
+{
+    public int a;
+    public int b;
+
+    public int Square()
+    {
+        return a * b;
+    }
+
+    public Rectangle()
+    {
+        a = 6;
+        b = 4;
+    }
+
+    public Rectangle(int side)
+    {
+        a = side;
+        b = side;
+    }
+
+    public Rectangle(int first, int second)
+    {
+        a = first;
+        b = second;
+    }
+    
+}
