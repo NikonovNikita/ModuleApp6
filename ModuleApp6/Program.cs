23ebf6439bc5a4cb689a8d9aee0fd5217c3b04f9
Nikita Nikita <jassie.jassie@yandex.ru>
2023-09-30 13:22:53 +0300

    1

--- ModuleApp6/Program.cs
+++ ModuleApp6/Program.cs
@@ -1,30 +1,45 @@
 ﻿using System;
-class Rectangle
+using System.Data;
+
+struct Data
 {
-    public int a;
-    public int b;
-
-    public int Square()
+    public string Name;
+    public int Length;
+    public int Version;
+    public int[] Array;
+}
+class Obj
+{
+    public string Name;
+    public bool IsAlive;
+    public int Weight;
+}
+class Program
+{
+    static void Main(string[] args)
     {
-        return a * b;
-    }
+        Data data = new() { Name = "Запись", Length = 10, Version = 1, Array = new int[] {15, 30} };
+        Obj obj = new() { Name = "Стол", IsAlive = false, Weight = 15 };
+        
+        var dataCopy = data;
+        var objCopy = obj;
 
-    public Rectangle()
-    {
-        a = 6;
-        b = 4;
-    }
+        data.Name = "Значение";
+        data.Length = 35;
+        data.Version = 2;
+        data.Array[0] = 0;
+        dataCopy.Name = "Проверка";
 
-    public Rectangle(int side)
-    {
-        a = side;
-        b = side;
-    }
+        obj.Name = "Кот";
+        obj.IsAlive = true;
+        obj.Weight = 3;
 
-    public Rectangle(int first, int second)
-    {
-        a = first;
-        b = second;
+        objCopy = new Obj { Name = obj.Name, IsAlive = obj.IsAlive, Weight = obj.Weight };
+
+        obj.Name = "Стол";
+        obj.IsAlive = false;
+        obj.Weight = 15;
+
+        Console.ReadKey();
     }
-    
 }
