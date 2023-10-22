7da6e9cd312300fe51395c6dfd14a792d7f76806
Nikita Nikita <jassie.jassie@yandex.ru>
2023-10-22 11:54:02 +0300

    Индексаторы

--- ModuleApp6/Program.cs
+++ ModuleApp6/Program.cs
@@ -1,65 +1,52 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Data;
 
-class User
+class Book
 {
-    private int age;
-    private string login;
-    private string email;
-
-    public string Login
+    public string Name;
+    public string Author;
+}
+class BookCollection
+{
+    private Book[] collection;
+    public BookCollection(Book[] collection)
+    {
+        this.collection = collection;
+    }
+    public Book this [int index]
     {
         get
         {
-            return login;
-        }
-        set
-        {
-            if (value.Length < 3)
+            if(index >= 0 && index < collection.Length) 
             {
-                Console.WriteLine("Логин не может быть менее 3 символов!");
+                return collection[index];      
             }
             else
             {
-                login = value;
+                return null;
+            }
+        }
+        private set
+        {
+            if (index >= 0 && index < collection.Length)
+            {
+                collection[index] = value;
             }
         }
     }
-    public string Email
+    public Book this[string name]
     {
         get
         {
-            return email;
-        }
-        set
-        {
-            if (!value.Contains('@'))
+            for(int i = 0; i < collection.Length; i++)
             {
-                Console.WriteLine("Неверный формат адреса почты!");
+                if (collection[i].Name == name)
+                {
+                    return collection[i];
+                }
             }
-            else
-            {
-                email = value;
-            }
-        }
-    }
-
-    public int Age
-    {
-        get
-        {
-            return age;
-        }
-        set
-        {
-            if (value < 18)
-            {
-                Console.WriteLine("Возраст должен быть не меньше 18!");
-            }
-            else
-            {
-                age = value;
-            }
+            return null;
         }
     }
 }
@@ -67,9 +54,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        User user = new();
-        user.Age = 21;
-        Console.WriteLine(user.Age);
+        var array = new Book[]
+        {
+            new Book
+            {
+                Name = "Мастер и Маргарита",
+                Author = "М. А. Булгаков"
+            },
+            new Book
+            {
+                Name = "Отцы и дети",
+                Author = "И. С. Тургенев"
+            },
+        };
+        BookCollection collection = new BookCollection(array);
+        Book book1 = collection[0];
+        Book book2 = collection[1];
+        var array1 = new Book[]
+        {
+            new Book{ Name = "Мастер и Маргарита", Author = "М. А. Булгаков"},
+            new Book{ Name = "Отцы и дети", Author = "И. С. Тургенев"},
+        };
+        BookCollection collection1 = new BookCollection(array1);
+        Book book3 = collection1[0];
+        Book book4 = collection1[1];
+        Book book5 = collection["Мастер и Маргарита"];
+        Console.ReadKey();
+
 
     }
 }
