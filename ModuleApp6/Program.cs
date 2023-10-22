7185f4764e86365429d5fd70407094fb99df0832
Nikita Nikita <jassie.jassie@yandex.ru>
2023-10-22 23:27:49 +0300

    Задание 7.5.9

--- ModuleApp6/Program.cs
+++ ModuleApp6/Program.cs
@@ -2,26 +2,41 @@
 using System.ComponentModel.Design;
 using System.Data;
 
-class Book
+static class IntExtensions
 {
-    public string Name;
-    public string Author;
-}
-class BookCollection
-{
-    private Book[] collection;
-    public BookCollection(Book[] collection)
+    public static int GetNegative(this int number)
     {
-        this.collection = collection;
-    }
-    public Book this [int index]
-    {
-        get
+        if(number < 0)
         {
-            if(index >= 0 && index < collection.Length) 
-            {
-                return collection[index];      
-            }
+            return number;
+        }
+        else
+        {
+            return - number;
+        }
+    }
+    public static int GetPositive(this int number)
+    {
+        if (number > 0)
+        {
+            return number;
+        }
+        else
+        {
+            return - number;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int num1 = 7;
+        int num2 = -13;
+        int num3 = 0;
 
+        Console.WriteLine(num1.GetPositive());
+        Console.WriteLine(num1.GetNegative());
+        Console.WriteLine(num2.GetPositive());
+        Console.WriteLine(num2.GetNegative());
+        Console.WriteLine(num3.GetPositive());
+        Console.WriteLine(num3.GetNegative());
     }
 }
