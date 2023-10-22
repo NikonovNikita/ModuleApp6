87416e1e0d520a3e41cb6fc663ac61727e0ab38c
Nikita Nikita <jassie.jassie@yandex.ru>
2023-10-22 22:21:08 +0300

    Задание 7.5.3

--- ModuleApp6/Program.cs
+++ ModuleApp6/Program.cs
@@ -22,65 +22,6 @@ class BookCollection
             {
                 return collection[index];      
             }
-            else
-            {
-                return null;
-            }
-        }
-        private set
-        {
-            if (index >= 0 && index < collection.Length)
-            {
-                collection[index] = value;
-            }
-        }
-    }
-    public Book this[string name]
-    {
-        get
-        {
-            for(int i = 0; i < collection.Length; i++)
-            {
-                if (collection[i].Name == name)
-                {
-                    return collection[i];
-                }
-            }
-            return null;
-        }
-    }
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        var array = new Book[]
-        {
-            new Book
-            {
-                Name = "Мастер и Маргарита",
-                Author = "М. А. Булгаков"
-            },
-            new Book
-            {
-                Name = "Отцы и дети",
-                Author = "И. С. Тургенев"
-            },
-        };
-        BookCollection collection = new BookCollection(array);
-        Book book1 = collection[0];
-        Book book2 = collection[1];
-        var array1 = new Book[]
-        {
-            new Book{ Name = "Мастер и Маргарита", Author = "М. А. Булгаков"},
-            new Book{ Name = "Отцы и дети", Author = "И. С. Тургенев"},
-        };
-        BookCollection collection1 = new BookCollection(array1);
-        Book book3 = collection1[0];
-        Book book4 = collection1[1];
-        Book book5 = collection["Мастер и Маргарита"];
-        Console.ReadKey();
-
 
     }
 }
